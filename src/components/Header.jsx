aff068c069bba82480bf63dce58f3d407487cdda
Juan Francisco <contrerasjf.0@hotmail.com>
2021-04-04 17:45:58 -0500

    feature(Context): Define the main context for the app
    * The App context was defined in the context directory.
    * The useInitialState hook was created to define the main state for the app with context.
    * The AppContext and useInitalState hook were implemented in the App route file.
    
    Resolve: C-28

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons'
+import { faShoppingBasket } from '@fortawesome/free-solid-svg-icons';
 import '../styles/components/Header.css';
 
 const Header = () => {
