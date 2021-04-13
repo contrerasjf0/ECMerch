a9920c2e748087467f0269f78fc3483afcf98154
Juan Francisco <contrerasjf.0@hotmail.com>
2021-04-13 00:14:26 -0500

    feature(GoogleAddress): Implement a custom hook to retrieve the geometry location
    * Map component was updated to define the data prop for geometry location
    * The useGoogleAddress hook was created.
    * The useGoogleAddress was implemented on Success view.
    * Axios library was installed.
    
    Resolve: C-38

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react';
 import AppContext from '../context/AppContext';
 import Map from '../components/Map';
+import useGoogleAddress from '../hooks/useGoogleAddress';
 import '../styles/components/Success.css';
 
 
@@ -8,6 +9,7 @@ const Success = () => {
   
   const { state } = useContext(AppContext);
   const { buyer } = state;
+  const location = useGoogleAddress(buyer[0].address);
 
   return (
     <div className="Success">
@@ -15,7 +17,7 @@ const Success = () => {
         <h2>{buyer.name}, Gracias por tu compra</h2>
         <span>Tu pedido lelgara en 3 dias a tu direccion:</span>
         <div className="Success-map">
-          <Map />
+          <Map data={location} />
         </div>
       </div>
     </div>
