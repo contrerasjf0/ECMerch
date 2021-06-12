8e934bded84ccc7d2278417d1ec5e92d7c4ae0f9
Juan Francisco <contrerasjf.0@hotmail.com>
2021-06-12 15:11:56 -0500

    config(Test): Configure Jest and Implement Footer test
    Resolve: C-JR-07

---
+++ src/__test__/components/Footer.test.js
@@ -0,0 +1,17 @@
+//import 'jsdom-global/register';
+import React from 'react'
+import { mount } from 'enzyme'
+import Footer from '../../components/Footer'
+
+describe('Test Footer component', () => {
+  const footer = mount(<Footer/>)
+  
+  it('Component Footer is rendered', () => {
+    
+    expect(footer.length).toEqual(1)
+  })
+
+  it('Render title', () => {
+    expect(footer.find('.Fotter-title').text()).toEqual('EC Merch')
+  })
+})
